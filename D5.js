c7a77780ea6c135416e790f07172554f9352d04b
IlBennys <119436512+IlBennys@users.noreply.github.com>
2022-12-09 16:09:39 +0100

    java ok 1

--- D5.js
+++ D5.js
@@ -9,49 +9,75 @@ REGOLE
 /* ESERCIZIO 1
     Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
 */
-const pets = ['dog', 'cat', 'hamster', 'redfish']
-
+console.log("-----ESERCIZIO 1-----")
+const pets = ["dog", "cat", "hamster", "redfish"]
+console.log(pets)
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
-
+console.log("-----ESERCIZIO 2-----")
+pets.sort()
+console.log(pets)
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
-
+console.log("-----ESERCIZIO 3-----")
+pets.reverse()
+console.log(pets)
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
+console.log("-----ESERCIZIO 4-----")
+pets.shift()
+console.log(pets)
 
+pets.push("redfish")
+console.log(pets)
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
 */
+console.log("-----ESERCIZIO 5-----")
+
 const cars = [
   {
-    brand: 'Ford',
-    model: 'Fiesta',
-    color: 'red',
-    trims: ['titanium', 'st', 'active'],
+    brand: "Ford",
+    model: "Fiesta",
+    color: "red",
+    trims: ["titanium", "st", "active"],
   },
   {
-    brand: 'Peugeot',
-    model: '208',
-    color: 'blue',
-    trims: ['allure', 'GT'],
+    brand: "Peugeot",
+    model: "208",
+    color: "blue",
+    trims: ["allure", "GT"],
   },
   {
-    brand: 'Volkswagen',
-    model: 'Polo',
-    color: 'black',
-    trims: ['life', 'style', 'r-line'],
+    brand: "Volkswagen",
+    model: "Polo",
+    color: "black",
+    trims: ["life", "style", "r-line"],
   },
 ]
-
+for (let i = 0; i < cars; i++) {
+  let macchine = cars[i]
+  macchine.licenseplate = "AY12345"
+  console.log(macchine)
+}
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
+console.log("-----ESERCIZIO 6-----")
 
+const colour = { cod: "#455", paint: "green", carbody: "metallized" }
+cars.push(colour)
+console.log(cars)
+
+for (let i = 0; i < cars; i++) {
+  let senzatrim = cars[i].trims
+  delete senzatrim
+  console.log(cars)
+}
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
